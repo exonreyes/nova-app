3f53593020a42901540e131b1e5d169f4c2f28d5
Pablo Reyes <exon1704@gmail.com>
2025-02-21 11:49:54 -0600

    añadir clase Ticket al código base

---
+++ domain/src/main/java/nova/domain/empresa/model/Ticket.java
@@ -0,0 +1,45 @@
+package nova.domain.empresa.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nova.core.actividad.Estatus;
+import nova.core.actividad.Reporte;
+import nova.core.persona.Persona;
+import nova.domain.actividad.model.Seguimiento;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ticket {
+    private Integer id;
+    private Unidad unidad;
+    private Reporte titulo;
+    private String descripcion;
+    private String folio;
+    private String agente;
+    private Boolean publicar;
+    private Estatus estatus;
+    private List<Persona> compartir;
+    private LocalDateTime creado;
+    private LocalDateTime actualizado;
+    private List<Seguimiento> seguimientos;
+    private AreaSeguimiento area;
+
+    public Ticket(Integer id, Unidad unidad, Reporte titulo) {
+        this.id = id;
+        this.unidad = unidad;
+        this.titulo = titulo;
+    }
+
+    public Ticket(Integer id, String agente, String estatus, String descripcion, LocalDateTime creado) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.agente = agente;
+        this.estatus = new Estatus(estatus);
+        this.creado = creado;
+    }
+}
